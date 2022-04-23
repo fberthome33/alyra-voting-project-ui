41ea6fe294a25295c990165c1dc7bcd52d2778ca
Fabien Berthomé <fabien.berthome.ext@veolia.com>
2022-04-23 18:19:38 +0200

    Updates

---
+++ precache-manifest.f95de3753192563e37bcfbd8e81579a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801d31211d0733f9a1b88792e072d35a",
+    "url": "/fberthome33/alyra-voting-project/index.html"
+  },
+  {
+    "revision": "456e3f79fbd360313807",
+    "url": "/fberthome33/alyra-voting-project/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "1826f95e6fee99f8c16d",
+    "url": "/fberthome33/alyra-voting-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "456e3f79fbd360313807",
+    "url": "/fberthome33/alyra-voting-project/static/js/2.e9980940.chunk.js"
+  },
+  {
+    "revision": "1826f95e6fee99f8c16d",
+    "url": "/fberthome33/alyra-voting-project/static/js/main.16e599cf.chunk.js"
+  },
+  {
+    "revision": "f9274fbc48165e8b8bb9",
+    "url": "/fberthome33/alyra-voting-project/static/js/runtime-main.120ecbd5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fberthome33/alyra-voting-project/precache-manifest.f95de3753192563e37bcfbd8e81579a2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fberthome33/alyra-voting-project/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
